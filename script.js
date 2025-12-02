d05d4cedaafe1046bddeeaa7697f46ef0334ef22
Fabian Ferreira <fab14an.fp@gmail.com>
2025-12-02 13:01:45 -0400

    Create operate function

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+let num1 = "";
+let num2 = "";
+let operator = "";
+
 function add(a, b) { return a + b; }
 function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 function divide(a, b) { 
     if (b === 0) return "Error: Division by zero";
     return a / b; 
+}
+
+function operate(operator, num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    switch (operator) {
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1, num2);
+        default: return null;
+    }
 }
